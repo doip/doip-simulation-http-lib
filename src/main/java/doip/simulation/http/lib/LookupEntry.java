6535f67191800abab323dbad86c4d6d0a8a589df
npyvovar <nataliya.pyvovar@expleogroup.com>
2023-11-20 12:19:30 +0100

    Added Classes Ecu.java, Modifier.java, LookupEntry.java

--- src/main/java/doip/simulation/http/lib/LookupEntry.java
+++ src/main/java/doip/simulation/http/lib/LookupEntry.java
@@ -1,14 +1,12 @@
 package doip.simulation.http.lib;
+
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class Gateway {
-	public String name;
-	public String url;
-	public String status;
-	public String error;
-	public List<Ecu> ecus;
-
+public class LookupEntry {
+    public String regex;
+    public String result;
+    public List<Modifier> modifiers;
 }
