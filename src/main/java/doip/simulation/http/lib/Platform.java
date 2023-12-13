bbaa69e70bcad45224d915dec53378003dd0c7fa
Marco Wehnert <marco.wehnert@gmx.de>
2023-12-13 00:57:39 +0000

    Added getter/setter methods to ServerInfo and Platform

--- src/main/java/doip/simulation/http/lib/Platform.java
+++ src/main/java/doip/simulation/http/lib/Platform.java
@@ -10,4 +10,29 @@ public class Platform {
     public String url;
     public String status;
     public List<Gateway> gateways;
+			
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public List<Gateway> getGateways() {
+		return gateways;
+	}
+	public void setGateways(List<Gateway> gateways) {
+		this.gateways = gateways;
+	}
 }
--- src/main/java/doip/simulation/http/lib/ServerInfo.java
+++ src/main/java/doip/simulation/http/lib/ServerInfo.java
@@ -7,4 +7,12 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ServerInfo {
     public List<Platform> platforms;
+    
+    public List<Platform> getPlatforms() {
+    	return platforms;
+    }
+    
+    public void setPlatforms(List<Platform> platforms) {
+    	this.platforms = platforms;
+    }
 }
